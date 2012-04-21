ce48192375435ed309e71b8361ee980ae77af377
SebastianN <SebastianN@NICKELSE1.emea.hpqcorp.net>
2012-04-21 16:01:56 +0200

    XML-Changes (so far)

--- src/edu/dhbw/sos/data/CourseManager.java
+++ src/edu/dhbw/sos/data/CourseManager.java
@@ -10,6 +10,8 @@
 package edu.dhbw.sos.data;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.xml.stream.*;
 
@@ -17,6 +19,7 @@ import edu.dhbw.sos.course.Course;
 import edu.dhbw.sos.course.student.EmptyPlace;
 import edu.dhbw.sos.course.student.IPlace;
 import edu.dhbw.sos.course.student.Student;
+import edu.dhbw.sos.helper.Matrix;
 import edu.dhbw.sos.helper.Parameter;
 
 
@@ -116,19 +119,25 @@ public class CourseManager {
 	
 	/* File format
 	 * <savefile>
-	 * 		<courses count="X">
-	 * 			<course name="KURSNAME" students="X">
-	 * 				foreach<student isempty="0" ODER isempty="1">
-	 * 					<attribute name="Tiredness" value="...">
-	 * 					<attribute name=" value="...">
-	 * 					<..........>
-	 * 				</student>
-	 * 			</course>
-	 * 		</courses>
-	 * 		<changevector>
-	 * 			<change val="...">
+	 * 	<courses count="X">
+	 * 		<course name="KURSNAME" students="X">
+	 * 			foreach<student isempty="0" ODER isempty="1">
+	 * 				<sAttribute name="Tiredness" value="...">
+	 * 				<sAttribute name=" value="...">
+	 * 				<..........>
+	 * 			</student>
+	 * 		</course>
+	 * 	</courses>
+	 * 	<changevector>
+	 * 		<matrow>
+	 * 			<mAttribute name="_its_name_" val="...">
 	 * 			........
-	 * 		</changevector>
+	 * 		</matrow>
+	 * 	</changevector>
+	 * 	<influencevector>
+	 * 		<iAttribute>
+	 * 	</influencevector>
+	 * 	
 	 * </savefile>
 	 */
 public void saveCourses() {
@@ -139,65 +148,74 @@ public void saveCourses() {
 		if(writer!=null) {
 			writer.writeStartDocument();
 			writer.writeStartElement("savefile");
+			if(courses!=null) {
 			
-			writer.writeStartElement("courses");
-			writer.writeAttribute("count", String.valueOf(courses.length) );
-			
-			for(int i=0;i<courses.length;i++) {
-				//Write for each course --> <course name="NAME" students="COUNT">
+				writer.writeStartElement("courses");
+				writer.writeAttribute("count", String.valueOf(courses.length) );
+				
+				for(int i=0;i<courses.length;i++) {
+					//Write for each course --> <course name="NAME" students="COUNT">
+						
+					IPlace[][] students = courses[i].getStudents();
 					
-				IPlace[][] students = courses[i].getStudents();
-				
-				writer.writeStartElement("course");	
-/**
- * FIXME:	writer.writeAttribute("name", courses[i].getName());
- */
-				writer.writeAttribute("x", String.valueOf(students[0].length));
-				writer.writeAttribute("y", String.valueOf(students.length));
-				
-				for(int j=0;j<students.length;j++) {
-					for(int k=0;k<students[j].length;k++) {
-						//For each student --> <student>
-						writer.writeStartElement("student");
-						
-						if(students[j][k].getClass().getName().contains("EmptyPlace")) {
-							writer.writeAttribute("isempty", "1"); //if it's empty --> no further information
-						} else {
-							writer.writeAttribute("isempty", "0");
-							Student currentStudent = (Student)students[j][k];
-							for(int m=0;m<currentStudent.getActualState().size();m++) {
-								//<attribute value="VALUE">
-								writer.writeStartElement("attribute");
-								writer.writeAttribute("name", currentStudent.getActualState().getTypeAt(m) );
-								writer.writeAttribute("value", String.valueOf( currentStudent.getActualState().getValueAt(m) ));
-								writer.writeEndElement();
-								//</attribute>
+					writer.writeStartElement("course");	
+	/**
+	 * FIXME:	writer.writeAttribute("name", courses[i].getName());
+	 */
+					writer.writeAttribute("x", String.valueOf(students[0].length));
+					writer.writeAttribute("y", String.valueOf(students.length));
+					
+					for(int j=0;j<students.length;j++) {
+						for(int k=0;k<students[j].length;k++) {
+							//For each student --> <student>
+							writer.writeStartElement("student");
+							
+							if(students[j][k].getClass().getName().contains("EmptyPlace")) {
+								writer.writeAttribute("isempty", "1"); //if it's empty --> no further information
+							} else {
+								writer.writeAttribute("isempty", "0");
+								Student currentStudent = (Student)students[j][k];
+								for(int m=0;m<currentStudent.getActualState().size();m++) {
+									//<attribute value="VALUE">
+									writer.writeStartElement("sAttribute");
+									writer.writeAttribute("name", currentStudent.getActualState().getTypeAt(m) );
+									writer.writeAttribute("value", String.valueOf( currentStudent.getActualState().getValueAt(m) ));
+									writer.writeEndElement();
+									//</attribute>
+								}
 							}
+							
+							writer.writeEndElement();
+							//</student>
 						}
-						
-						writer.writeEndElement();
-						//</student>
 					}
+					writer.writeEndElement(); 
+					//</course>
 				}
 				writer.writeEndElement(); 
-				//</course>
+				//</courses>
+					
+	
+				if(courses[0].getInfluence()!=null && courses[0].getInfluence().getParameterMatrix()!=null) {
+					writer.writeStartElement("changematrix");
+					Matrix parMatrix = courses[0].getInfluence().getParameterMatrix();
+					writer.writeAttribute("rows_columns", String.valueOf(parMatrix.size()));
+					
+					for(int row=0;row<parMatrix.size();row++) {
+						writer.writeStartElement("mat_row");
+						for(int col=0;col<parMatrix.size();col++) {
+							writer.writeStartElement("mAttribute");
+							writer.writeAttribute("mat_elem", parMatrix.getElementAt(col, row).getType());
+							writer.writeAttribute("value", "VALUE");
+							writer.writeEndElement();
+						}
+						writer.writeEndElement();
+					//</change>
+					}
+					writer.writeEndElement();
+					//</changevector>
+				}
 			}
-			writer.writeEndElement(); 
-			//</courses>
-				
-
-				
-			writer.writeStartElement("changematrix");
-			writer.writeAttribute("count", "5");
-			for(int i=0;i<5;i++) {
-				writer.writeStartElement("change");
-				writer.writeAttribute("name", String.valueOf(i));
-				writer.writeAttribute("value", "VALUE");
-				writer.writeEndElement();
-			//</change>
-			}
-			writer.writeEndElement();
-			//</changevector>
 			writer.writeEndElement();
 			writer.writeEndDocument();
 			
@@ -209,6 +227,10 @@ public void saveCourses() {
 		ex.printStackTrace();
 	}
 }
+
+private IPlace[][] allocStudents( int rows, int columns ) {
+	return new IPlace[rows][columns];
+}
 	
 public void loadCourses() {
 	XMLInputFactory factory = XMLInputFactory.newInstance();
@@ -218,8 +240,16 @@ public void loadCourses() {
 		
 		int courseIdx = -1;
 		IPlace[][] students = null;
+		Matrix changeMat = null;
+		
 		int rows = 0, columns = 0;
-		int max_rows = 0, max_columns = 0;
+		
+		int matrix_row = -1;
+		int matrix_col = 0;
+		
+		LinkedList<String> paramname = new LinkedList<String>();
+		int[][] matVals = null;
+		
 		while(reader.hasNext()) {
 			reader.next(); //Next element
 			
@@ -236,30 +266,44 @@ public void loadCourses() {
 /**
  * FIXME:			courses[courseIdx].setName( reader.getAttributeValue(0) );
  */
-						max_columns = Integer.parseInt(reader.getAttributeValue(1));
-						max_rows = Integer.parseInt(reader.getAttributeValue(2));
-						students = new EmptyPlace[max_rows][max_columns];
+						students = allocStudents( Integer.parseInt(reader.getAttributeValue(1)), Integer.parseInt(reader.getAttributeValue(2)));
 						
 					} else if(tagname.contentEquals("student")) {
+						//isEmpty="0"
 						if(Integer.parseInt(reader.getAttributeValue(0))==0) {
 /**
  * FIXME:				students[y][x] = new Student();
  */
-							columns++;
-							if(columns>=max_columns) { //
-								columns = 0;
-								rows++;
-							}
+						} else { //isEmpty="1"
+/**
+ * FIXME:				students[rows][columns] = new EmptyPlace();
+ */
 						}
-					} else if(tagname.contentEquals("attribute")) {
+						columns++;
+						if(columns>=students[0].length) { //
+							columns = 0;
+							rows++;
+						}
+					} else if(tagname.contentEquals("sAttribute")) {
 						Student curStudent = (Student)students[rows][columns];
 						Parameter p = new Parameter( reader.getAttributeValue(0), Integer.parseInt( reader.getAttributeValue(1) ) );
 						curStudent.addParamToStudent(p);
-					} else if(tagname.contentEquals("changevector")) {
-						System.out.println("Changevectorcount: " + reader.getAttributeValue(0));
-					} else if(tagname.contentEquals("change")) {
-						System.out.println("Changename: " + reader.getAttributeValue(0));
-						System.out.println("Changeval: " + reader.getAttributeValue(1));
+					} else if(tagname.contentEquals("changematrix")) {
+						
+						int size = Integer.parseInt(reader.getAttributeValue(0));
+						matVals = new int[size][size];
+						
+					} else if(tagname.contentEquals("mat_row")) {
+						matrix_row++;
+						matrix_col=0;
+						
+					} else if(tagname.contentEquals("mAttribute")) {
+						matVals[matrix_row][matrix_col] = Integer.parseInt(reader.getAttributeValue(0)); //Integer value
+						paramname.add(reader.getAttributeValue(1));
+						matrix_col++;
+						
+					} else if(tagname.contentEquals("influencevector")) {
+						
 					}
 				} catch( Exception ex ) {
 					//...Useless exceptions for every little thing.
