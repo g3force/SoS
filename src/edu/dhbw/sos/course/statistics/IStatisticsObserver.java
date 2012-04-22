29b05870f5c62e09a2d6b3476d7d151f06e36402
Nicolai Ommer <nicolai.ommer@gmail.com>
2012-04-22 02:08:15 +0200

    communication with interfaces

---
+++ src/edu/dhbw/sos/course/statistics/IStatisticsObserver.java
@@ -0,0 +1,22 @@
+/*
+ * *********************************************************
+ * Copyright (c) 2012 - 2012, DHBW Mannheim
+ * Project: SoS
+ * Date: Apr 22, 2012
+ * Author(s): NicolaiO
+ * 
+ * *********************************************************
+ */
+package edu.dhbw.sos.course.statistics;
+
+/**
+ * TODO NicolaiO, add comment!
+ * - What should this type do (in one sentence)?
+ * - If not intuitive: A simple example how to use this class
+ * 
+ * @author NicolaiO
+ * 
+ */
+public interface IStatisticsObserver {
+	void updateStatistics();
+}
