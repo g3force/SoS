b4099996a61af1ffc83bccd14df7c10d76113850
Daniel Andres <daniel@laptop>
2012-04-23 16:44:15 +0200

    buttons for play

--- src/edu/dhbw/sos/gui/plan/PlayBtn.java
+++ src/edu/dhbw/sos/gui/plan/PlayBtn.java
@@ -25,13 +25,25 @@ import javax.swing.JButton;
 public class PlayBtn extends JButton {
 	
 	private static final long	serialVersionUID	= -3859433203503408752L;
-
+	private Mode					mode					= Mode.PAUSE;
+	
+	
 	/**
 	 * Live Button Constructor with icon.
 	 * @author andres
 	 */
 	public PlayBtn() {
 		super("PL");
+		toggle();
+	}
+	
+	private enum Mode {
+		PLAY,
+		PAUSE
+	}
+	
+	
+	private void setPlay() {
 		URL iconUrlLive = getClass().getResource("/res/icons/play.png");
 		if (iconUrlLive != null) {
 			ImageIcon icon = new ImageIcon(iconUrlLive);
@@ -41,6 +53,40 @@ public class PlayBtn extends JButton {
 			this.setIcon(newIcon);
 			this.setText("");
 			this.setBorderPainted(false);
+		} else {
+			this.setText("PL");
+		}
+		mode = Mode.PAUSE;
+	}
+	
+	
+	private void setPause() {
+		URL iconUrlLive = getClass().getResource("/res/icons/pause.png");
+		if (iconUrlLive != null) {
+			ImageIcon icon = new ImageIcon(iconUrlLive);
+			Image img = icon.getImage();
+			Image newimg = img.getScaledInstance(20, 20, java.awt.Image.SCALE_SMOOTH);
+			ImageIcon newIcon = new ImageIcon(newimg);
+			this.setIcon(newIcon);
+			this.setText("");
+			this.setBorderPainted(false);
+		} else {
+			this.setText("PA");
+		}
+		mode = Mode.PLAY;
+	}
+	
+	
+	public void toggle() {
+		switch (mode) {
+			case PLAY:
+				setPause();
+				break;
+			case PAUSE:
+				setPlay();
+				break;
+			default:
+				break;
 		}
 	}
 }
