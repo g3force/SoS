fa06c8d04c826351af73536e88df96b3fa7f21c4
Benedikt Zirbes <benedikt.zirbes@me.com>
2012-04-24 20:58:13 +0200

    added XMLParam helper object, redesigned Suggestion object for better XStream compatibility. work still in progress, do not use yet!

---
+++ src/edu/dhbw/sos/helper/XMLParam.java
@@ -0,0 +1,51 @@
+/* 
+ * *********************************************************
+ * Copyright (c) 2012 - 2012, DHBW Mannheim
+ * Project: SoS
+ * Date: Apr 24, 2012
+ * Author(s): bene
+ *
+ * *********************************************************
+ */
+package edu.dhbw.sos.helper;
+
+/**
+ * This is only a helper object to make the stored xml code of Suggestion objects better human readable.
+ * 
+ * @author bene
+ * 
+ */
+public class XMLParam {
+	private String	name;
+	private float	influence;
+	private float	rangeLowerEnd;
+	private float	rangeUpperEnd;
+	
+	
+	public XMLParam(String name, float influence, float rangeLowerEnd, float rangeUpperEnd) {
+		this.name = name;
+		this.influence = influence;
+		this.rangeLowerEnd = rangeLowerEnd;
+		this.rangeUpperEnd = rangeUpperEnd;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	
+	
+	public float getInfluence() {
+		return influence;
+	}
+	
+	
+	public float getRangeLowerEnd() {
+		return rangeLowerEnd;
+	}
+	
+	
+	public float getRangeUpperEnd() {
+		return rangeUpperEnd;
+	}
+}
