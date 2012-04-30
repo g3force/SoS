f085c65af0bf76291b6a7d9ecb852719c01cd2c7
Daniel Andres <daniel@laptop>
2012-04-30 08:59:31 +0200

    hier

---
+++ src/edu/dhbw/sos/simulation/ISimUntilObserver.java
@@ -0,0 +1,22 @@
+/* 
+ * *********************************************************
+ * Copyright (c) 2012 - 2012, DHBW Mannheim
+ * Project: SoS
+ * Date: Apr 30, 2012
+ * Author(s): andres
+ *
+ * *********************************************************
+ */
+package edu.dhbw.sos.simulation;
+
+/**
+ * TODO andres, add comment!
+ * - What should this type do (in one sentence)?
+ * - If not intuitive: A simple example how to use this class
+ * 
+ * @author andres
+ * 
+ */
+public interface ISimUntilObserver {
+	void updateSimUntil(boolean state);
+}
