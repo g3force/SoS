1ba8e761f0684ccec9cf8eef58095b846a2c037f
PEACE121 <dirk@klm-on.de>
2012-09-07 11:46:04 -0400

    todos dirk

--- src/edu/dhbw/sos/course/lecture/BlockType.java
+++ src/edu/dhbw/sos/course/lecture/BlockType.java
@@ -138,7 +138,6 @@ public enum BlockType {
 	
 	/**
 	 * @return the type to compare with EInfluence type
-	 *         TODO Dirk bei Beschwerden bitte an mich wenden ;)
 	 * @author dirk
 	 */
 	public EInfluenceType getEInfluenceType() {
